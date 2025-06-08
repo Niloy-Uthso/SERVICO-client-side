e141156c61fcf883088dfa759c9d09c71649108e
Niloy-Uthso <niloyuthso16@gmail.com>
2025-06-08 19:02:06 +0600

    first commit

---
+++ service/src/pages/Allservices.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { NavLink, useLoaderData, useLocation } from 'react-router';
+import Eachgroup from '../components/Eachgroup';
+import { valueContext } from '../Rootlayout';
+import Lottie from 'lottie-react';
+ 
+const Allservices = () => {
+     const groups=useLoaderData()
+    //  const location=useLocation()
+    const {theme}=useContext(valueContext)
+     
+    return (
+       <div className='flex events flex-col p-4 pt-8 items-center  gap-10'>
+         
+            <h1 className={`text-4xl ${theme?``:`text-white`} font-bold`}>All groups</h1>
+            <div data-aos="fade-up" className='grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8'>
+                {
+                    groups.map(group=>(
+                        <Eachservice key={group._id} group={group}></Eachservice>
+                    ))
+                }
+            </div>
+            <NavLink to={'/'}>
+                      <button class="btn btn-dash btn-accent">Back to Home</button>
+            </NavLink>
+            
+        </div>
+    );
+};
+
+export default Allservices;
